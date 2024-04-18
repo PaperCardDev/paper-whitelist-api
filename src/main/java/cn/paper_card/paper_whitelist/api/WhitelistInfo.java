d39cfc8779ff6dfccd67381f4e7a55d3e45af943
Paper99 <1398872296@qq.com>
2024-04-18 15:54:38 +0800

    5.0.1 no update

--- src/main/java/cn/paper_card/paper_whitelist/api/WhitelistInfo.java
+++ src/main/java/cn/paper_card/paper_whitelist/api/WhitelistInfo.java
@@ -2,9 +2,15 @@ package cn.paper_card.paper_whitelist.api;
 
 import java.util.UUID;
 
+/**
+ * 白名单信息
+ *
+ * @param name       玩家名称
+ * @param userId     玩家UUID
+ * @param remark     备注信息
+ * @param createTime 添加时间，秒级时间戳
+ */
 public record WhitelistInfo(
-
-        // 玩家名
         String name,
 
         // 玩家UUID
